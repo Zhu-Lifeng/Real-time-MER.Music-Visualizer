ebde68e7948c55c5c5f16d3d6d6ff9646cdec1a2
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2024-05-20 17:50:22 +0100

    Update main_default.py

--- main_default.py
+++ main_default.py
@@ -11,21 +11,35 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
+# MIDI: Output processing
 midi_file = '/content/drive/MyDrive/MSC Project/MIDI-Unprocessed_SMF_02_R1_2004_01-05_ORIG_MID--AUDIO_02_R1_2004_05_Track05_wav.midi'
 midi_stream = converter.parse(midi_file)
 
-audio_file='/content/drive/MyDrive/MSC Project/MIDI-Unprocessed_SMF_02_R1_2004_01-05_ORIG_MID--AUDIO_02_R1_2004_05_Track05_wav.wav'
-y, sr = librosa.load(audio_file)
-
-notes=[]
+notes_midi=[]
 for element in midi_stream.flatten().notes:
   if isinstance(element, note.Note):
-    notes.append([[element.pitch.nameWithOctave],element.duration.quarterLength])
+    notes_midi.append([[element.pitch.nameWithOctave],element.duration.quarterLength])
   if isinstance(element,chord.Chord):
     chords=[]
     for N in element.notes:
       chords.append(N.pitch.nameWithOctave)
-    notes.append([chords,element.duration.quarterLength])
+    notes_midi.append([chords,element.duration.quarterLength])
 
-for pitch, duration in notes[0:10]:
+for pitch, duration in notes_midi[0:10]:
     print(pitch,duration)
+
+
+# Audio: Input processing
+audio_file='/content/drive/MyDrive/MSC Project/MIDI-Unprocessed_SMF_02_R1_2004_01-05_ORIG_MID--AUDIO_02_R1_2004_05_Track05_wav.wav'
+y, sr = librosa.load(audio_file)
+
+pitches, magnitudes = librosa.core.piptrack(y=y, sr=sr, threshold=0.01)
+notes_audio=[]
+for j in range(pitches.shape[1]):
+  N=[]
+  for i in range(pitches.shape[0]):
+    if pitches[i,j] != 0:
+      N.append([pitches[i,j],magnitudes[i,j]])
+  notes_audio.append(N)
+for X in notes_audio[10:50]:
+    print(X)
