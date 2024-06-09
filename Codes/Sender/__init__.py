3f4942dfa30f042be82b5ea919891889a21d28c7
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2024-06-09 14:34:28 +0100

    Add files via upload

---
+++ Codes/Sender/__init__.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, jsonify,flash
+import time
+import pandas as pd
+import numpy as np
+import json
+import requests
+import librosa
+import os
+def Sender_Creation():
+    app = Flask(__name__)
+    app.secret_key = '19980706'
+    app.config['UPLOAD_FOLDER'] = 'uploads'
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    #audio, sr = librosa.load('MIDI-Unprocessed_Chamber2_MID--AUDIO_09_R3_2018_wav--1.wav', sr=44100)
+
+
+    @app.route('/')
+    def index():
+        return render_template('S_index.html')
+
+    @app.route('/upload', methods=['POST'])
+    def upload_file():
+        global audio, sr
+        if 'file' not in request.files:
+            return 'No file part'
+        file = request.files['file']
+        if file.filename == '':
+            return 'No selected file'
+        if file:
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            audio, sr = librosa.load( file_path , sr=44100)
+            flash('File successfully uploaded and loaded')
+            return render_template('S_index.html')
+
+
+    def secure_filename(filename):
+        return filename
+
+
+    @app.route('/audio_fragment_send', methods=['GET', 'POST'])
+    def send_data():
+        global audio, sr
+        if request.method == 'POST':
+            target_ip = '127.0.0.1' #request.form.get('target_ip')
+            port = '8001' #request.form.get('port')
+            url = f'http://{target_ip}:{port}/audio_fragment_receive'
+            headers = {'Content-Type': 'application/json'}
+            responses = []
+            ssr = 4410
+            for t in range(len(audio)//ssr):  # 检查 notes_midi 是否为空
+                data = audio[t*ssr:t*ssr+ssr].tolist()
+                try:
+                    response = requests.post(url, json=data)
+                    responses.append((t, response.status_code))
+                    print(f'第 {t} 条数据发送成功')
+                except requests.exceptions.RequestException as e:
+                    print(f"发送数据失败: {e}")
+                    responses.append((t, e))
+                #time.sleep(5)  # 根据响应时间需求调整
+            #requests.post(url,json=999999)
+            return jsonify(responses)  # 返回所有发送状态的汇总
+
+    return app
