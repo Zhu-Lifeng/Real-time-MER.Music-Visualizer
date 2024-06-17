fe684296c5b8ae8ecec07569c8d2ce62c8a08697
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2024-06-17 16:45:53 +0100

    Delete Codes/Client directory

--- Codes/Client/main_Client.py
+++
@@ -1,6 +0,0 @@
-from Back_Stage import Client_Creation
-from flask_socketio import SocketIO, emit
-app=Client_Creation()
-
-if __name__ == '__main__':
-    app.run(debug = True,host = '0.0.0.0', port = 8002)
