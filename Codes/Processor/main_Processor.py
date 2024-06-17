6a355a7b42f677e8937203adfa2e3427b3432785
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2024-06-17 18:27:12 +0100

    Update main_Processor.py

--- Codes/Processor/main_Processor.py
+++ Codes/Processor/main_Processor.py
@@ -5,4 +5,4 @@ app=Processor_Creation()
 if __name__ == '__main__':
     with app.app_context():
 
-        app.run(debug=True,host='0.0.0.0',port=8001)
+        app.run(debug=True,host='0.0.0.0',port=8080)
