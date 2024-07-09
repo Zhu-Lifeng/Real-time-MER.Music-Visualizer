c15407dbc8863d22341c4178a72729192a6f0d19
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2024-07-09 14:29:11 +0100

    Create main_serverless.py

---
+++ main_serverless.py
@@ -0,0 +1,9 @@
+from Back_Stage import Processor_Creation
+import ssl
+app=Processor_Creation()
+
+if __name__ == '__main__':
+  context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+  context.load_cert_chain('ssl_cert.crt', 'ssl_private.key', '1998')
+
+  app.run(debug=True,  ssl_context=context, host='0.0.0.0',port=8080)
