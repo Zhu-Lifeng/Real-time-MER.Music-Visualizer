4dd3594c0d324581454d89759a62c5a84d9dbb56
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2024-07-10 13:32:18 +0100

    Delete main_serverless.py

--- main_serverless.py
+++
@@ -1,7 +0,0 @@
-from Back_Stage import Processor_Creation
-
-
-app=Processor_Creation()
-if __name__ == '__main__':
-    with app.app_context():
-      app.run(debug=True,  host='0.0.0.0',port=8080)
