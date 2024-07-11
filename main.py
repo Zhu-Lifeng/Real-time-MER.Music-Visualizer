52440cef06a870acbec85b70ce593d12d9a0879f
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2024-07-11 17:33:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from Back_Stage import Processor_Creation
+from Back_Stage import Processor_Creation, db
 
-app=Processor_Creation()
+app = Processor_Creation()
 
 if __name__ == '__main__':
     with app.app_context():
-
-        app.run(debug=True,host='0.0.0.0',port=8080)
+        db.drop_all()
+        db.create_all()
+    app.run(debug=True,host='0.0.0.0',port=8080)
